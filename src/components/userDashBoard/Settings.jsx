372945dbc3310b19eadebb5e32c15e25e5bcd885
shivanshu7898 <sonishivanshu7898163335@gmail.com>
2026-07-03 20:45:56 +0530

    some change in setting page

--- src/components/userDashBoard/Settings.jsx
+++ src/components/userDashBoard/Settings.jsx
@@ -8,6 +8,9 @@ const UserDashboard = () => {
       <div>Welcome Back!! {user.fullName}</div>
       <div>Welcome Back!! {user.email}</div>
       <div>Welcome Back!! {user.phone}</div>
+
+
+      
       <div className="w-24 h-24 rounded-full overflow-hidden">
         <img
           src={user.photo}
